6ca99ea940577c6e20063334422e770b4c1a64fc
peder aarseth krøgenes <pederkr@gmail.com>
2019-06-24 09:45:54 +0200

    New suggestion.

---
+++ NumberGuess/HumanPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NumberGuess
+{
+    public class HumanPlayer
+    {
+         public static short AskForNumber()
+         {
+            Console.WriteLine("Let's play a game... please input a number between 1 and 1000, and we guarantee that we will be able to guess it within ten guesses.");
+            short numberToGuess = (short)Int32.Parse(Console.ReadLine());
+            return numberToGuess;
+         }
+
+
+
+    }
+}
